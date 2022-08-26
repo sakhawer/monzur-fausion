4496cd981e0e0c99eb571fb8efc3636d4026c760
unknown <nshossenbd@gmail.com>
2022-08-26 14:32:51 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# monzurfausion
+# monzurfausion
+# monzur-fausion
---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(function() {
+    $('.material-tooltip-main').tooltip({
+        template: '<div class="tooltip md-tooltip"><div class="tooltip-arrow md-arrow"></div><div class="tooltip-inner md-inner"></div></div>'
+    });
+})
+
+
+for (i=0; i<10; i++){
+    console.log(i)
+}
